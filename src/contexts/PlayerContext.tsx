3e046436b0185ae8f509c1f44745968dba00b36b
Felipe Pasqua <felipepasquarodrigues@gmail.com>
2021-04-27 01:34:12 -0300

    Final Commit

---
+++ src/contexts/PlayerContext.tsx
@@ -0,0 +1,152 @@
+import { createContext, useState, ReactNode, useContext } from 'react'
+
+
+type Episode = {
+    title: string;
+    members: string;
+    thumbnail: string;
+    duration: number;
+    url: string;
+
+};
+
+type PlayerContextData = {
+    episodeList: Episode[];
+    currentEpisodeIndex: number;
+    isPlaying: boolean;
+    isLooping: boolean;
+    isShuffling: boolean;
+    play: (episode: Episode) => void;
+    setPlayingState: (state: boolean) => void;
+    togglePlay: () => void;
+    toggleLoop: () => void;
+    toggleShuffle: () => void;
+    playList: (list: Episode[], index: number) => void;
+    playNext: () => void;
+    playPrevious: () => void;
+    clearPlayerState: () => void;
+    hasNext: boolean;
+    hasPrevious: boolean;
+};
+
+
+export const PLayerContext = createContext({} as PlayerContextData);
+
+
+
+type PlayerContextProviderProps = {
+    children: ReactNode;
+}
+
+
+export function PlayerContextProvider({ children }: PlayerContextProviderProps) {
+
+    const [episodeList, setEpisodeList] = useState([]);
+    const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [isLooping, setIsLooping] = useState(false);
+    const [isShuffling, setIsShuffling] = useState(false);
+
+
+    function play(episode: Episode) {
+        setEpisodeList([episode])
+        setCurrentEpisodeIndex(0);
+        setIsPlaying(true);
+    }
+
+    function playList(list: Episode[], index: number) {
+        setEpisodeList(list);
+        setCurrentEpisodeIndex(index);
+        setIsPlaying(true);
+
+    }
+
+
+
+    function togglePlay() {
+        setIsPlaying(!isPlaying);
+    }
+
+
+    function toggleLoop() {
+        setIsLooping(!isLooping);
+    }
+
+    function toggleShuffle() {
+        setIsShuffling(!isShuffling);
+    }
+
+
+    function setPlayingState(state: boolean) {
+        setIsPlaying(state);
+    }
+
+
+    function clearPlayerState() {
+        setEpisodeList([]);
+        setCurrentEpisodeIndex(0);
+    }
+
+
+    const hasPrevious = currentEpisodeIndex > 0;
+    const hasNext = isShuffling || (currentEpisodeIndex + 1) < episodeList.length;
+
+
+    function playNext() {
+
+        if (isShuffling) {
+            const nextRandomEpisodeIndex = Math.floor(Math.random() * episodeList.length)
+
+            setCurrentEpisodeIndex(nextRandomEpisodeIndex);
+
+        } else if (hasNext) {
+            setCurrentEpisodeIndex
+        }
+
+
+        if (hasNext) {
+            setCurrentEpisodeIndex(currentEpisodeIndex + 1);
+        }
+
+    }
+
+
+    function playPrevious() {
+        if (hasPrevious) {
+            setCurrentEpisodeIndex(currentEpisodeIndex - 1);
+        }
+    }
+
+
+
+
+    return (
+
+        <PLayerContext.Provider
+            value={{
+                episodeList,
+                currentEpisodeIndex,
+                play,
+                isPlaying,
+                togglePlay,
+                playNext,
+                playPrevious,
+                playList,
+                setPlayingState,
+                hasNext,
+                isShuffling,
+                hasPrevious,
+                toggleLoop,
+                isLooping,
+                toggleShuffle,
+                clearPlayerState,
+
+            }}>
+            {children}
+        </PLayerContext.Provider>
+    )
+}
+
+export const usePlayer = () => {
+    return useContext(PLayerContext);
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,19 +5,25 @@ import { Player } from '../components/Player';
 
 
 import styles from '../styles/app.module.scss';
+import { PlayerContextProvider } from '../contexts/PlayerContext';
+
+
 
 
 
 function MyApp({ Component, pageProps }) {
-  return (
 
-    <div className={styles.wrapper}>
-      <main>
-        <Header />
-        <Component {...pageProps} />
-      </main>
-      <Player />
-    </div>
+  return (
+    <PlayerContextProvider>
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContextProvider>
+
   )
 }
 
